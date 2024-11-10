a5c8ec5294054cb1b21753743cf64e69e7f497d5
SohailaSalama <sohailasalama903@gmail.com>
2024-11-10 16:29:46 +0200

    Update Testlab_6.py

--- lab_6.py
+++ lab_6.py
@@ -1,14 +1,7 @@
 # lab_6
-import unittest
-from calculator import Calculator 
+ 
 
-def test_add(self):
-    result=Calculator.add(3, 7)
-    self.assertEqual(result, 10)
-  
-    result = Calculator. add (-1, 1) 
-    self.assertEqual(result, 0)
-  
-    result = Calculator.add(-1, -1)
-    self.assertEqual(result, -2)
+class Calc
+def add(a, b):
+    return a + b
   
